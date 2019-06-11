ebbe78a59391617c55e126a2047ee75afb0b380b
Alex Beltran <Alex.e.beltran@gmail.com>
2019-06-11 10:57:24 -0700

    Update tag.go
    Remove whitespace from exclude

--- git/tag.go
+++ git/tag.go
@@ -37,7 +37,7 @@ func (g Git) PushTag(t tag.Tag) (err error) {
 func runGitDescribe() (string, error) {
 	// The following glob(7) pattern is not perfect. It will match things like v1.4badstring.8
 	// But it narrows down the results by a good bit. It will exclude prerelease tags such as `v1.4.8-rc-1`
-	cmd := exec.Command("git", "describe", "--abbrev=0", "--tags", "--match=v[0-9]*\\.[0-9]*\\.[0-9]*", "--exclude=v[0-9] *\\.[0 - 9]*\\.[0 - 9]*-*")
+	cmd := exec.Command("git", "describe", "--abbrev=0", "--tags", "--match=v[0-9]*\\.[0-9]*\\.[0-9]*", "--exclude=v[0-9]*\\.[0-9]*\\.[0-9]*-*")
 	// TODO: if we find a tag, but it's invalid (human created), we should retry and find the one previous.
 	// Probably with a `git describe <bad tag that was found>`
 	return runCommand(cmd)
